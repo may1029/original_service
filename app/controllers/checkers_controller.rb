9cb79a8d9bfed309e33a8bf94d91346e973d9465
may1029 <mymy.a1029@gmail.com>
2017-02-27 21:47:23 +0900

    show same height model

--- app/controllers/checkers_controller.rb
+++ app/controllers/checkers_controller.rb
@@ -5,7 +5,8 @@ class CheckersController < ApplicationController
 
   def result
     @bmi = 1.0 * params[:weight].to_i / ((params[:height].to_i / 100.0)  ** 2)
-    render text: @bmi.round(2)
+    @model = PersonalDatabase.where(height: params[:height])
+    # render text: @bmi.round(2)
     #BMIの計算
     #PersonalDBと身長の参照
   end
