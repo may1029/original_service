abcf815407744c54f438bf731dafe88547887089
may1029 <mymy.a1029@gmail.com>
2017-02-27 15:57:13 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+person1 = Personal_databases.create(name: "Tsubasa Masuwaka", height: 150, weight: 35, image: "/Users/miki/onboarding2017/original_service/app/assets/images/tsubasa.jpeg")
+person2 = Personal_databases.create(name: "Yuko Oshima", height: 152, weight: 43, image: "/Users/miki/onboarding2017/original_service/app/assets/images/yuko.jpg")
+person3 = Personal_databases.create(name: "Ai Takahashi", height: 154, weight: 42, image: "/Users/miki/onboarding2017/original_service/app/assets/images/ao.jpg")
+person4 = Personal_databases.create(name: "Airi Taira", height: 155, weight: 45, image: "/Users/miki/onboarding2017/original_service/app/assets/images/profile.jpg")
+person5 = Personal_databases.create(name: "Satomi Ishihara", height: 157, weight: 45, image: "/Users/miki/onboarding2017/original_service/app/assets/images/satomi.jpg")
+person6 = Personal_databases.create(name: "Rino Sashihara", height: 157, weight: 43.9, image: "/Users/miki/onboarding2017/original_service/app/assets/images/rino.jpg")
+person7 = Personal_databases.create(name: "Keiko Kitagawa", height: 160, weight: 42, image: "/Users/miki/onboarding2017/original_service/app/assets/images/keiko.jpg")
+person8 = Personal_databases.create(name: "Kasumi Arimura", height: 160, weight: 46, image: "/Users/miki/onboarding2017/original_service/app/assets/images/kasumi.jpg")
+person9 = Personal_databases.create(name: "Mirei Kiritani", height: 163.5, weight: 39.5, image: "/Users/miki/onboarding2017/original_service/app/assets/images/mirei.jpg")
+person10 = Personal_databases.create(name: "Rola", height: 165, weight: 48, image: "/Users/miki/onboarding2017/original_service/app/assets/images/rola.jpg")
+person11 = Personal_databases.create(name: "Yui Aragaki", height: 168, weight: 50, image: "/Users/miki/onboarding2017/original_service/app/assets/images/yui.jpg")
 
-person1 = Personal_databases.create(name: "Airi Taira", height: 155, weight: 45, image: "/Users/miki/onboarding2017/original_service/app/assets/images/profile.jpg")
+
