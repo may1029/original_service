945dc454d8c7ce8b57b63877365f71a2de5225af
may1029 <mymy.a1029@gmail.com>
2017-03-05 19:12:46 +0900

    edit layout and show judgement for BMI

--- app/controllers/checkers_controller.rb
+++ app/controllers/checkers_controller.rb
@@ -5,6 +5,14 @@ class CheckersController < ApplicationController
 
   def result
     @bmi = 1.0 * params[:weight].to_i / ((params[:height].to_i / 100.0)  ** 2)
+    if @bmi < 18.4 then
+      @judge = "Too thin"
+    elsif @bmi > 18.5 && @bmi < 24.9 then
+      @judge = "Just"
+    else
+      @judge = "Fat!"
+    end
+
     @model = PersonalDatabase.where(height: params[:height])
     # render text: @bmi.round(2)
     #BMIの計算
